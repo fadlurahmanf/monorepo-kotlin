c85c8b7169697a9685c3cc1753028ddd8d80ee1b
fadlurahmanf <tffajari@gmail.com>
2023-09-01 14:44:35 +0700

    custom button

---
+++ mapp_ui/src/main/java/com/fadlurahmanf/mapp_ui/custom/MappButton.kt
@@ -0,0 +1,54 @@
+package com.fadlurahmanf.mapp_ui.custom
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import com.fadlurahmanf.mapp_ui.R
+
+class MappButton(context: Context, attributeSet: AttributeSet) :
+    LinearLayout(context, attributeSet) {
+
+    private var attributes: TypedArray
+    private var button: Button
+
+    private var text: String
+    private var active: Boolean
+
+    init {
+        inflate(context, R.layout.cust_mapp_button, this)
+        attributes = context.obtainStyledAttributes(attributeSet, R.styleable.MappButton)
+
+        button = findViewById(R.id.btn)
+
+        text = attributes.getString(R.styleable.MappButton_text) ?: ""
+        active = attributes.getBoolean(R.styleable.MappButton_active, true)
+
+        setup()
+    }
+
+    private fun setup() {
+        setText(text)
+        setActive(active)
+    }
+
+    private fun setActive(active: Boolean) {
+        if (active) {
+            button.background =
+                ContextCompat.getDrawable(context, R.drawable.rounded_green_button_background)
+        } else {
+            button.background =
+                ContextCompat.getDrawable(context, R.drawable.rounded_grey_button_background)
+        }
+    }
+
+    private fun setText(text: String) {
+        button.text = text
+
+        button.setOnClickListener {
+            println("TES TES")
+        }
+    }
+}
