0465caad2d1449f22041b2e48cfeb061974e94bc
fadlurahmanf <tffajari@gmail.com>
2023-11-04 06:20:48 +0700

    fix collapse toolbar layout

--- bebas_onboarding/src/main/java/com/fadlurahmanf/bebas_onboarding/presentation/welcome/TncActivity.kt
+++ bebas_onboarding/src/main/java/com/fadlurahmanf/bebas_onboarding/presentation/welcome/TncActivity.kt
@@ -1,12 +1,17 @@
 package com.fadlurahmanf.bebas_onboarding.presentation.welcome
 
 import android.text.Html
+import android.util.Log
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.fadlurahmanf.bebas_api.network_state.NetworkState
-import com.fadlurahmanf.bebas_onboarding.databinding.ActivityTncBinding
+import com.fadlurahmanf.bebas_onboarding.R
+import com.fadlurahmanf.bebas_onboarding.databinding.ActivityTnc2Binding
 import com.fadlurahmanf.bebas_onboarding.presentation.BaseOnboardingActivity
 import javax.inject.Inject
 
-class TncActivity : BaseOnboardingActivity<ActivityTncBinding>(ActivityTncBinding::inflate) {
+class TncActivity : BaseOnboardingActivity<ActivityTnc2Binding>(ActivityTnc2Binding::inflate) {
 
     @Inject
     lateinit var viewModel: TncViewModel
