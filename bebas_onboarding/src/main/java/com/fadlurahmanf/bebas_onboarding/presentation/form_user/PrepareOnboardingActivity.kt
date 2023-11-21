5eb148d9f578a7e270e55d1aba0dd6c5bfc2393c
fadlurahmanf <tffajari@gmail.com>
2023-11-21 11:15:00 +0700

    setup prepare onboarding activity

--- bebas_onboarding/src/main/java/com/fadlurahmanf/bebas_onboarding/presentation/form_user/PrepareOnboardingActivity.kt
+++ bebas_onboarding/src/main/java/com/fadlurahmanf/bebas_onboarding/presentation/form_user/PrepareOnboardingActivity.kt
@@ -3,10 +3,15 @@ package com.fadlurahmanf.bebas_onboarding.presentation.form_user
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fadlurahmanf.bebas_onboarding.R
+import com.fadlurahmanf.bebas_onboarding.databinding.ActivityPrepareOnboardingBinding
+import com.fadlurahmanf.bebas_onboarding.presentation.BaseOnboardingActivity
 
-class PrepareOnboardingActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_prepare_onboarding)
+class PrepareOnboardingActivity : BaseOnboardingActivity<ActivityPrepareOnboardingBinding>(ActivityPrepareOnboardingBinding::inflate) {
+    override fun injectActivity() {
+
+    }
+
+    override fun setup() {
+
     }
 }
