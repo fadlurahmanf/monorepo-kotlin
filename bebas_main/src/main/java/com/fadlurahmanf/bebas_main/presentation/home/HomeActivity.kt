9dceb06499da25fbe721bbd489d1ee70d483e02c
fadlurahmanf <tffajari@gmail.com>
2023-12-15 19:22:20 +0700

    setup

--- bebas_main/src/main/java/com/fadlurahmanf/bebas_main/presentation/home/HomeActivity.kt
+++ bebas_main/src/main/java/com/fadlurahmanf/bebas_main/presentation/home/HomeActivity.kt
@@ -3,10 +3,14 @@ package com.fadlurahmanf.bebas_main.presentation.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fadlurahmanf.bebas_main.R
+import com.fadlurahmanf.bebas_main.databinding.ActivityHomeBinding
+import com.fadlurahmanf.bebas_main.presentation.BaseMainActivity
 
-class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+class HomeActivity : BaseMainActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate) {
+
+    override fun injectActivity() {
+    }
+
+    override fun setup() {
     }
 }
