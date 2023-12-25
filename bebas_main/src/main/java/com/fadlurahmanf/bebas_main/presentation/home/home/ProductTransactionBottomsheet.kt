fc1f3559538b0b37a084a3e5bbb9fe450e9687e6
fadlurahmanf <tffajari@gmail.com>
2023-12-25 09:39:24 +0700

    fix bottomhseet transaction menu

--- bebas_main/src/main/java/com/fadlurahmanf/bebas_main/presentation/home/home/ProductTransactionBottomsheet.kt
+++ bebas_main/src/main/java/com/fadlurahmanf/bebas_main/presentation/home/home/ProductTransactionBottomsheet.kt
@@ -1,12 +1,14 @@
 package com.fadlurahmanf.bebas_main.presentation.home.home
 
 import android.os.Build
+import androidx.recyclerview.widget.GridLayoutManager
 import com.fadlurahmanf.bebas_main.R
 import com.fadlurahmanf.bebas_main.data.dto.argument.ProductTransactionMenuArgument
 import com.fadlurahmanf.bebas_main.data.dto.model.home.SubProductTransactionMenuModel
 import com.fadlurahmanf.bebas_main.data.dto.model.home.ProductTransactionMenuModel
 import com.fadlurahmanf.bebas_main.databinding.BottomsheetProductTransactionBinding
 import com.fadlurahmanf.bebas_main.presentation.home.adapter.ProductTransactionMenuAdapter
+import com.fadlurahmanf.bebas_main.presentation.home.adapter.SubProductTransactionMenuAdapter
 import com.fadlurahmanf.bebas_ui.bottomsheet.BaseBottomsheet
 
 class ProductTransactionBottomsheet :
@@ -18,10 +20,12 @@ class ProductTransactionBottomsheet :
 
     lateinit var bottomsheetArgument: ProductTransactionMenuArgument
 
-    private var productTransaction: ArrayList<SubProductTransactionMenuModel> = arrayListOf()
-    private var transactionMenus: ArrayList<ProductTransactionMenuModel> = arrayListOf()
+    private var subProductTransactionMenu: ArrayList<SubProductTransactionMenuModel> = arrayListOf()
+    private var productTransactionMenu: ArrayList<ProductTransactionMenuModel> = arrayListOf()
+    private var selectedProductTransactionMenu: ProductTransactionMenuModel? = null
 
     private lateinit var productTransactionMenuAdapter: ProductTransactionMenuAdapter
+    private lateinit var subProductTransactionMenuAdapter: SubProductTransactionMenuAdapter
 
     override fun setup() {
         val p0arg = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -35,32 +39,49 @@ class ProductTransactionBottomsheet :
         }
 
         bottomsheetArgument = p0arg
-        transactionMenus = ArrayList(bottomsheetArgument.transactionMenus.map {
+        productTransactionMenu = ArrayList(bottomsheetArgument.transactionMenus.map {
+
+            val isSelected = it.productMenuId == bottomsheetArgument.selectedProductMenuId
+            if (isSelected) {
+                selectedProductTransactionMenu = it
+            }
+
             it.copy(
                 isSelected = it.productMenuId == bottomsheetArgument.selectedProductMenuId
             )
         })
 
-        generateProductTransactionMenus()
         productTransactionMenuAdapter = ProductTransactionMenuAdapter(
             type = ProductTransactionMenuAdapter.Type.BOTTOMSHEET_TRANSACTION_MENU_LABEL
         )
-        productTransactionMenuAdapter.setList(transactionMenus)
+        productTransactionMenuAdapter.setList(productTransactionMenu)
         binding.rvProductMenu.adapter = productTransactionMenuAdapter
+
+        generateSubProductTransactionMenuBasedOnSelectedProductTransactionMenu()
+
+        val gridLm = GridLayoutManager(requireContext(), 4)
+        subProductTransactionMenuAdapter = SubProductTransactionMenuAdapter()
+        subProductTransactionMenuAdapter.setList(subProductTransactionMenu)
+        binding.rvSubProduct.adapter = subProductTransactionMenuAdapter
+        binding.rvSubProduct.layoutManager = gridLm
     }
 
-    private fun generateProductTransactionMenus() {
+    private fun generateSubProductTransactionMenuBasedOnSelectedProductTransactionMenu() {
         val menus = arrayListOf(
             SubProductTransactionMenuModel(
                 subProductMenuId = "PREPAID_PLN",
                 subProductMenuLabel = R.string.electricity_denom,
                 subProductImageMenu = R.drawable.ic_subproduct_tokenlistrik,
-                productTransactionMenu = transactionMenus.firstOrNull {
+                productTransactionMenu = productTransactionMenu.firstOrNull {
                     it.productMenuId == "PURCHASE"
                 } ?: othersMenu
             )
         )
-        productTransaction = menus
+
+        subProductTransactionMenu.clear()
+        subProductTransactionMenu.addAll(ArrayList(menus.filter {
+            it.productTransactionMenu.productMenuId == selectedProductTransactionMenu?.productMenuId
+        }.toList()))
     }
 
     private val othersMenu = ProductTransactionMenuModel(
